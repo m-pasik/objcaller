117719479b7e7570f2f410a1d81fd63418886b16
m-pasik <m.pasik.dev@gmail.com>
2023-08-26 02:57:10 +0200

    Added comments to call.c

--- src/call.c
+++ src/call.c
@@ -3,22 +3,28 @@
 
 void call(void* fptr, char *fstring, ...)
 {
-    va_list args;
-    va_start(args, fstring);
-    
+    /* Arrays of integer and float arguments to be passed in registers */
     uint64_t *iargs = calloc(6, sizeof(uint64_t));
-    double *dargs = calloc(8, sizeof(double));
-
+    double *fargs = calloc(8, sizeof(double));
+    
+    /* List of arguments to be passed in stack */
     size_t sargs_len = 16;
     uint64_t *sargs = malloc(sargs_len * sizeof(uint64_t));
 
-    size_t ii = 0, di = 0, si = 0;
+    size_t ii = 0, fi = 0, si = 0; /* indexes of iargs, dargs, sargs */
+
+    /*
+     * Read varargs based on format string.
+     */
+
+    va_list args;
+    va_start(args, fstring);
 
     for (char* c = strchr(fstring, '%'); c != NULL; c = strchr(++c, '%')) {
         switch (*++c) {
-            case 'i':
-                if (strncmp(++c, "32", 2) == 0) {
-                    int arg = va_arg(args, uint32_t);
+            case 'i':   /* Integer */
+                if (strncmp(++c, "32", 2) == 0) {   /* 32-bit */
+                    uint32_t arg = va_arg(args, uint32_t);
                     if (ii < 6) {
                         iargs[ii++] = (uint64_t)arg;
                     } else {
@@ -28,8 +34,9 @@ void call(void* fptr, char *fstring, ...)
                         }
                         sargs[si++] = (uint64_t)arg;
                     }
-                } else if (strncmp(c, "64", 2) == 0) {
-                    long long int arg = va_arg(args, uint64_t);
+                } else if (strncmp(c, "64", 2) == 0) {  /* 64-bit */
+                    uint64_t arg = va_arg(args, uint64_t);
+
                     if (ii < 6) {
                         iargs[ii++] = arg;
                     } else {
@@ -40,8 +47,9 @@ void call(void* fptr, char *fstring, ...)
                         sargs[si++] = arg;
                     }
 
-                } else if (strncmp(c, "128", 3) == 0) {
+                } else if (strncmp(c, "128", 3) == 0) { /* 128-bit */
                     __uint128_t arg = va_arg(args, __uint128_t);
+
                     if (ii < 5) {
                         iargs[ii++] = (uint64_t)(arg);
                         iargs[ii++] = (uint64_t)(arg >> 64);
@@ -57,10 +65,14 @@ void call(void* fptr, char *fstring, ...)
                 }
                 break;
             case 'f':
-                if (strncmp(++c, "64", 2) == 0) {
+                /* 
+                 * Float
+                 */
+                if (strncmp(++c, "64", 2) == 0) {  /* 64-bit */
                     double arg = va_arg(args, double);
-                    if (di < 8) {
-                        dargs[di++] = arg;
+
+                    if (fi < 8) {
+                        fargs[fi++] = arg;
                     } else {
                         if (si >= sargs_len) {
                             sargs_len *= 2;
@@ -70,8 +82,9 @@ void call(void* fptr, char *fstring, ...)
                     }
 
                 }
-                else if (strncmp(c, "128", 3) == 0) {
+                else if (strncmp(c, "128", 3) == 0) {   /* 128-bit */
                     long double arg = va_arg(args, long double);
+
                     if (si >= sargs_len - 1) {
                         sargs_len *= 2;
                         sargs = realloc(sargs, sargs_len * sizeof(uint64_t));
@@ -87,17 +100,24 @@ void call(void* fptr, char *fstring, ...)
 
     va_end(args);
 
-    size_t len = si * 8;
-    if (si % 2)
-        len += 8;
+    /*
+     * Passing arguments and calling function
+     */
 
     asm volatile(
+        /* Set r12 to si * 8. */ 
+        "movq %[si], %%r12;"
+        "shlq $3, %%r12;"
+
+        /* If si is odd add 8 byte offset to the stack and add 8 to r12. */
         "movq %[si], %%rax;"
         "and $1, %%rax;"
         "cmp $0, %%rax;"
         "je cmp;"
         "lea -8(%%rsp), %%rsp;"
+        "addq $8, %%r12;"
         
+        /* Push all arguments from sargs to the stack. */
         "jmp cmp;"
         "loop:"
         "push (%[sargs]);"
@@ -107,15 +127,17 @@ void call(void* fptr, char *fstring, ...)
         "cmp $0, %[si];"
         "jne loop;"
 
-        "movq (%[dargs]), %%xmm0;"
-        "movq 8(%[dargs]), %%xmm1;"
-        "movq 16(%[dargs]), %%xmm2;"
-        "movq 24(%[dargs]), %%xmm3;"
-        "movq 32(%[dargs]), %%xmm4;"
-        "movq 40(%[dargs]), %%xmm5;"
-        "movq 48(%[dargs]), %%xmm6;"
-        "movq 56(%[dargs]), %%xmm7;"
+        /* Move floating-point arguments to xmm registers. */
+        "movq (%[fargs]), %%xmm0;"
+        "movq 8(%[fargs]), %%xmm1;"
+        "movq 16(%[fargs]), %%xmm2;"
+        "movq 24(%[fargs]), %%xmm3;"
+        "movq 32(%[fargs]), %%xmm4;"
+        "movq 40(%[fargs]), %%xmm5;"
+        "movq 48(%[fargs]), %%xmm6;"
+        "movq 56(%[fargs]), %%xmm7;"
         
+        /* Move integer arguments to integer registers. */
         "movq (%[iargs]), %%rdi;"
         "movq 8(%[iargs]), %%rsi;"
         "movq 16(%[iargs]), %%rdx;"
@@ -123,16 +145,19 @@ void call(void* fptr, char *fstring, ...)
         "movq 32(%[iargs]), %%r8;"
         "movq 40(%[iargs]), %%r9;"
 
+        /* Call the function. */
         "call *%[fptr];"
-        "add %[len], %%rsp;"
+
+        /* Bring stack back to its original state. */
+        "add %%r12, %%rsp;"
         : 
         : [si] "r"(si), [sargs] "r"(&sargs[si-1]),
-          [dargs] "r"(dargs), [iargs] "r"(iargs),
-          [fptr] "m"(fptr), [len] "m"(len)
-        : "rax", "rdi", "rsi", "rdx", "rcx", "r8", "r9"
+          [fargs] "r"(fargs), [iargs] "r"(iargs),
+          [fptr] "m"(fptr)
+        : "rax", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r12"
     );
 
     free(iargs);
-    free(dargs);
+    free(fargs);
     free(sargs);
 }
