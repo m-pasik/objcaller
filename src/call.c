8e4328de475560d81122d49e96811b435c70710b
m-pasik <m.pasik.dev@gmail.com>
2023-08-26 02:21:24 +0200

    Useless change

--- src/call.c
+++ src/call.c
@@ -88,57 +88,49 @@ void call(void* fptr, char *fstring, ...)
     va_end(args);
 
     size_t len = si * 8;
-    if (si % 2) {
+    if (si % 2)
         len += 8;
-        asm volatile("lea -8(%rsp), %rsp");
-    }
-
-    for (; si > 0; --si)
-        asm volatile("push %0;" : : "m"(sargs[si - 1]));
-
-    asm volatile (
-        "movq (%0), %%xmm0\n"
-        "movq 8(%0), %%xmm1\n"
-        "movq 16(%0), %%xmm2\n"
-        "movq 24(%0), %%xmm3\n"
-        "movq 32(%0), %%xmm4\n"
-        "movq 40(%0), %%xmm5\n"
-        "movq 48(%0), %%xmm6\n"
-        "movq 56(%0), %%xmm7\n"
-        :
-        : "r"(dargs)
-    );
-    
-    //asm volatile(
-    //    "movdqu (%0), %%xmm0;"
-    //    "movdqu 16(%0), %%xmm1;"
-    //    "movdqu 32(%0), %%xmm2;"
-    //    "movdqu 48(%0), %%xmm3;"
-    //    "movdqu 64(%0), %%xmm4;"
-    //    "movdqu 80(%0), %%xmm5;"
-    //    "movdqu 96(%0), %%xmm6;"
-    //    "movdqu 112(%0), %%xmm7;"
-    //    :
-    //    : "r"(dargs)
-    //);
 
     asm volatile(
-        "movq (%0), %%rdi;"
-        "movq 8(%0), %%rsi;"
-        "movq 16(%0), %%rdx;"
-        "movq 24(%0), %%rcx;"
-        "movq 32(%0), %%r8;"
-        "movq 40(%0), %%r9;"
-        :
-        : "r"(iargs)
-        : "rdi", "rsi", "rdx", "rcx", "r8", "r9"
-    );
+        "movq %[si], %%rax;"
+        "and $1, %%rax;"
+        "cmp $0, %%rax;"
+        "je cmp;"
+        "lea -8(%%rsp), %%rsp;"
+        
+        "jmp cmp;"
+        "loop:"
+        "push (%[sargs]);"
+        "lea -8(%[sargs]), %[sargs];"
+        "subq $1, %[si];"
+        "cmp:"
+        "cmp $0, %[si];"
+        "jne loop;"
 
-    asm volatile("call *%0;"
-                 "add %1, %%rsp;"
-                 :
-                 : "m"(fptr), "m"(len)
-                 : "rdi", "rsi", "rdx", "rcx", "r8", "r9");
+        "movq (%[dargs]), %%xmm0;"
+        "movq 8(%[dargs]), %%xmm1;"
+        "movq 16(%[dargs]), %%xmm2;"
+        "movq 24(%[dargs]), %%xmm3;"
+        "movq 32(%[dargs]), %%xmm4;"
+        "movq 40(%[dargs]), %%xmm5;"
+        "movq 48(%[dargs]), %%xmm6;"
+        "movq 56(%[dargs]), %%xmm7;"
+        
+        "movq (%[iargs]), %%rdi;"
+        "movq 8(%[iargs]), %%rsi;"
+        "movq 16(%[iargs]), %%rdx;"
+        "movq 24(%[iargs]), %%rcx;"
+        "movq 32(%[iargs]), %%r8;"
+        "movq 40(%[iargs]), %%r9;"
+
+        "call *%[fptr];"
+        "add %[len], %%rsp;"
+        : 
+        : [si] "r"(si), [sargs] "r"(&sargs[si-1]),
+          [dargs] "r"(dargs), [iargs] "r"(iargs),
+          [fptr] "m"(fptr), [len] "m"(len)
+        : "rax", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "cc", "memory"
+    );
 
     free(iargs);
     free(dargs);
